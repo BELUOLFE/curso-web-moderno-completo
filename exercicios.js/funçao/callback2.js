7dcb9beb465f3984ab8f51575512a3fa114985db
luiz <luizasgo3@gmail.com>
2021-05-11 10:27:08 -0300

    aula nova

--- exercicios.js/funçao/callback2.js
+++ exercicios.js/funçao/callback2.js
@@ -12,11 +12,11 @@ const notasBaixas1 = []
 
  // Com callback
  const notasBaixas2 = notas.filter(function (nota) {
-     return nota > 7
+     return nota < 7
  })
 
  console.log(notasBaixas2)
 
- const notasMenorQue7 = nota => nota > 7
+ const notasMenorQue7 = nota => nota < 7
  const notasBaixas3 = notas.filter(notasMenorQue7)
  console.log(notasBaixas3)
