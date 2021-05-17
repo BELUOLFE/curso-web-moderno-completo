2b3adcac34a0b2a78451aa49a5bbd99fde4e7fec
luiz <luizasgo3@gmail.com>
2021-05-17 11:40:59 -0300

    aula nova

--- exercicios.js/objeto/heranca2.js
+++ exercicios.js/objeto/heranca2.js
@@ -1,5 +1,6 @@
 // Cadeia de protótipos (prototype chain)
 Object.prototype.attr0 = '0' // não faça isso em casa!
+
 const avo = { attr1: 'A'}
 const pai = { __proto__: avo, attr2: 'B', attr3: '3'}
 const filho = { __proto__: pai, attr3: 'C'}
