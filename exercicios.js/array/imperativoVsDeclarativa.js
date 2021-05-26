6e4d701ee10f10716fff894e2efae6b951d4a090
luiz <luizasgo3@gmail.com>
2021-05-26 10:55:48 -0300

    aula nova

--- exercicios.js/array/imperativoVsDeclarativa.js
+++ exercicios.js/array/imperativoVsDeclarativa.js
@@ -13,5 +13,8 @@ console.log(total1 / alunos.length)
 //  Declarativo
 const getNota = alunos => alunos.nota
 const soma = (total , atual) => total + atual
+
 const total2 = alunos.map(getNota).reduce(soma)
 console.log(total2 / alunos.length)
+
+// select codigo, nome, email from clientes where ativo = 1 
