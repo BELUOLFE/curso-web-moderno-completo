a9efae27b1aff5e6c90e0fe3869063fce6afb2d6
luiz <luizasgo3@gmail.com>
2021-06-07 12:10:14 -0300

    aula nova

--- exercicios.js/node/instanciaCliente.js
+++ exercicios.js/node/instanciaCliente.js
@@ -7,3 +7,7 @@ const contadorD = require('./instanciaNova')()
 contadorA.inc()
 contadorA.inc()
 console.log(contadorA.valor, contadorB.valor)
+
+contadorC.inc()
+contadorC.inc()
+console.log(contadorC.valor, contadorD.valor)
